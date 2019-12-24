0bea5d8b84c44da8a5f68f76c16a2c9c00512e28
futa <24775037+futa@users.noreply.github.com>
2019-12-24 02:57:07 -0800

    Update site

---
+++ scripts/calendar.js
@@ -0,0 +1,122 @@
+export default function drawCalendar(dateData, type) {
+  let weeksInMonth = function(month) {
+    let m = d3.timeMonth.floor(month);
+    return d3.timeWeeks(d3.timeWeek.floor(m), d3.timeMonth.offset(m, 1)).length;
+  };
+
+  let minDate = d3.min(dateData, d => {
+    return new Date(d.day);
+  });
+  let maxDate = d3.max(dateData, d => {
+    return new Date(d.day);
+  });
+
+  let cellMargin = 2,
+    cellSize = 20;
+
+  let day = d3.timeFormat("%w"),
+    week = d3.timeFormat("%U"),
+    format = d3.timeFormat("%m-%d-%Y"),
+    titleFormat = d3.utcFormat("%a, %d-%b"),
+    monthName = d3.timeFormat("%B"),
+    months = d3.timeMonth.range(d3.timeMonth.floor(minDate), maxDate);
+
+  let svg = d3
+    .select(`#calendar-${type}`)
+    .selectAll("svg")
+    .data(months)
+    .enter()
+    .append("svg")
+    .attr("class", "month")
+    .attr("height", cellSize * 7 + cellMargin * 8 + 20) // the 20 is for the month labels
+    .attr("width", function(d) {
+      let columns = weeksInMonth(d);
+      return cellSize * columns + cellMargin * (columns + 1);
+    })
+    .append("g");
+
+  svg
+    .append("text")
+    .attr("class", "month-name")
+    .attr("y", cellSize * 7 + cellMargin * 8 + 15)
+    .attr("x", function(d) {
+      let columns = weeksInMonth(d);
+      return (cellSize * columns + cellMargin * (columns + 1)) / 2;
+    })
+    .attr("text-anchor", "middle")
+    .text(function(d) {
+      return monthName(d);
+    });
+
+  let rect = svg
+    .selectAll("rect.day")
+    .data(function(d, i) {
+      return d3.timeDays(d, new Date(d.getFullYear(), d.getMonth() + 1, 1));
+    })
+    .enter()
+    .append("rect")
+    .attr("class", "day")
+    .attr("width", cellSize)
+    .attr("height", cellSize)
+    .attr("rx", 3)
+    .attr("ry", 3) // rounded corners
+    .attr("fill", "#eaeaea") // default light grey fill
+    .attr("y", function(d) {
+      return day(d) * cellSize + day(d) * cellMargin + cellMargin;
+    })
+    .attr("x", function(d) {
+      return (
+        (week(d) - week(new Date(d.getFullYear(), d.getMonth(), 1))) *
+          cellSize +
+        (week(d) - week(new Date(d.getFullYear(), d.getMonth(), 1))) *
+          cellMargin +
+        cellMargin
+      );
+    })
+    .on("mouseover", function(d) {
+      d3.select(this).classed("hover", true);
+    })
+    .on("mouseout", function(d) {
+      d3.select(this).classed("hover", false);
+    })
+    .datum(format);
+
+  rect.append("title").text(function(d) {
+    return titleFormat(new Date(d));
+  });
+
+  let lookup = d3
+    .nest()
+    .key(function(d) {
+      return d.day;
+    })
+    .rollup(function(leaves) {
+      return d3.sum(leaves, function(d) {
+        return parseInt(d.count);
+      });
+    })
+    .object(dateData);
+
+  let scale = d3
+    .scaleLinear()
+    .domain(
+      d3.extent(dateData, function(d) {
+        return parseInt(d.count);
+      })
+    )
+    .range([0.1, 1]); // the interpolate used for color expects a number in the range [0,1] but i don't want the lightest part of the color scheme
+
+  rect
+    .filter(function(d) {
+      return d in lookup;
+    })
+    .style("fill", d => {
+      return type === "wipe"
+        ? d3.interpolateReds(scale(lookup[d]))
+        : d3.interpolateBlues(scale(lookup[d]));
+    })
+    .select("title")
+    .text(function(d) {
+      return titleFormat(new Date(d)) + ":  " + lookup[d];
+    });
+}
---
+++ scripts/gantt.js
@@ -0,0 +1,409 @@
+//from this stackexchange question: http://stackoverflow.com/questions/1890203/unique-for-arrays-in-javascript
+function checkUnique(arr) {
+  let hash = {},
+    result = [];
+  for (let i = 0, l = arr.length; i < l; ++i) {
+    if (!hash.hasOwnProperty(arr[i])) {
+      //it works with objects! in FF, at least
+      hash[arr[i]] = true;
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
+
+let taskArray = [
+  {
+    task: "conceptualize",
+    type: "Garuda",
+    startTime: "2013-1-28", //year/month/day
+    endTime: "2013-2-1",
+    details: "This actually didn't take any conceptualization"
+  },
+
+  {
+    task: "sketch",
+    type: "Garuda",
+    startTime: "2013-2-1",
+    endTime: "2013-2-6",
+    details: "No sketching either, really"
+  },
+
+  {
+    task: "color profiles",
+    type: "Garuda",
+    startTime: "2013-2-6",
+    endTime: "2013-2-9"
+  },
+
+  {
+    task: "HTML",
+    type: "Ifrit",
+    startTime: "2013-2-2",
+    endTime: "2013-2-6",
+    details: "all three lines of it"
+  },
+
+  {
+    task: "write the JS",
+    type: "Ifrit",
+    startTime: "2013-2-6",
+    endTime: "2013-2-9"
+  },
+
+  {
+    task: "advertise",
+    type: "Ifrit",
+    startTime: "2013-2-9",
+    endTime: "2013-2-12",
+    details: "This counts, right?"
+  },
+
+  {
+    task: "spam links",
+    type: "Titan",
+    startTime: "2013-2-12",
+    endTime: "2013-2-14"
+  },
+  {
+    task: "eat",
+    type: "Predation",
+    startTime: "2013-2-8",
+    endTime: "2013-2-13",
+    details: "All the things"
+  },
+
+  {
+    task: "crying",
+    type: "Anniliation",
+    startTime: "2013-2-13",
+    endTime: "2013-2-16"
+  }
+];
+let w = 800;
+let h = 400;
+let categories = new Array();
+
+for (let i = 0; i < taskArray.length; i++) {
+  categories.push(taskArray[i].type);
+}
+
+let catsUnfiltered = categories; //for vert labels
+
+categories = checkUnique(categories);
+
+let svg = d3
+  .selectAll("#chart")
+  //.selectAll("svg")
+  .append("svg")
+  .attr("width", w)
+  .attr("height", h)
+  .attr("class", "svg");
+
+let dateFormat = d3.timeParse("%Y-%m-%d");
+let timeScale = d3
+  .scaleTime()
+  .domain([
+    d3.min(taskArray, function(d) {
+      return dateFormat(d.startTime);
+    }),
+    d3.max(taskArray, function(d) {
+      return dateFormat(d.endTime);
+    })
+  ])
+  .range([0, 800 - 150]);
+
+export default function drawGantt(tasks, pageWidth, pageHeight, categories) {
+  let barHeight = 20;
+  let gap = barHeight + 4;
+  let topPadding = 75;
+  let sidePadding = 75;
+
+  let colorScale = d3
+    .scaleLinear()
+    .domain([0, categories.length])
+    .range(["#00B9FA", "#F95002"])
+    .interpolate(d3.interpolateHcl);
+
+  makeGrid(sidePadding, topPadding, pageWidth, pageHeight);
+  drawRects(
+    tasks,
+    gap,
+    topPadding,
+    sidePadding,
+    barHeight,
+    colorScale,
+    pageWidth,
+    pageHeight
+  );
+  vertLabels(gap, topPadding, sidePadding, barHeight, colorScale);
+}
+
+function drawRects(
+  theArray,
+  theGap,
+  theTopPad,
+  theSidePad,
+  theBarHeight,
+  theColorScale,
+  w,
+  h
+) {
+  let bigRects = svg
+    .append("g")
+    .selectAll("rect")
+    .data(theArray)
+    .enter()
+    .append("rect")
+    .attr("x", 0)
+    .attr("y", function(d, i) {
+      return i * theGap + theTopPad - 2;
+    })
+    .attr("width", function(d) {
+      return w - theSidePad / 2;
+    })
+    .attr("height", theGap)
+    .attr("stroke", "none")
+    .attr("fill", function(d) {
+      for (let i = 0; i < categories.length; i++) {
+        if (d.type == categories[i]) {
+          return d3.rgb(theColorScale(i));
+        }
+      }
+    })
+    .attr("opacity", 0.2);
+
+  let rectangles = svg
+    .append("g")
+    .selectAll("rect")
+    .data(theArray)
+    .enter();
+
+  let innerRects = rectangles
+    .append("rect")
+    .attr("rx", 3)
+    .attr("ry", 3)
+    .attr("x", function(d) {
+      return timeScale(dateFormat(d.startTime)) + theSidePad;
+    })
+    .attr("y", function(d, i) {
+      return i * theGap + theTopPad;
+    })
+    .attr("width", function(d) {
+      return (
+        timeScale(dateFormat(d.endTime)) - timeScale(dateFormat(d.startTime))
+      );
+    })
+    .attr("height", theBarHeight)
+    .attr("stroke", "none")
+    .attr("fill", function(d) {
+      for (let i = 0; i < categories.length; i++) {
+        if (d.type == categories[i]) {
+          return d3.rgb(theColorScale(i));
+        }
+      }
+    });
+
+  let rectText = rectangles
+    .append("text")
+    .text(function(d) {
+      return d.task;
+    })
+    .attr("x", function(d) {
+      return (
+        (timeScale(dateFormat(d.endTime)) -
+          timeScale(dateFormat(d.startTime))) /
+          2 +
+        timeScale(dateFormat(d.startTime)) +
+        theSidePad
+      );
+    })
+    .attr("y", function(d, i) {
+      return i * theGap + 14 + theTopPad;
+    })
+    .attr("font-size", 11)
+    .attr("text-anchor", "middle")
+    .attr("text-height", theBarHeight)
+    .attr("fill", "#fff");
+
+  // rectText
+  //   .on("mouseover", function(e) {
+  //     // console.log(this.x.animVal.getItem(this));
+  //     let tag = "";
+
+  //     if (d3.select(this).data()[0].details != undefined) {
+  //       tag =
+  //         "Task: " +
+  //         d3.select(this).data()[0].task +
+  //         "<br/>" +
+  //         "Type: " +
+  //         d3.select(this).data()[0].type +
+  //         "<br/>" +
+  //         "Starts: " +
+  //         d3.select(this).data()[0].startTime +
+  //         "<br/>" +
+  //         "Ends: " +
+  //         d3.select(this).data()[0].endTime +
+  //         "<br/>" +
+  //         "Details: " +
+  //         d3.select(this).data()[0].details;
+  //     } else {
+  //       tag =
+  //         "Task: " +
+  //         d3.select(this).data()[0].task +
+  //         "<br/>" +
+  //         "Type: " +
+  //         d3.select(this).data()[0].type +
+  //         "<br/>" +
+  //         "Starts: " +
+  //         d3.select(this).data()[0].startTime +
+  //         "<br/>" +
+  //         "Ends: " +
+  //         d3.select(this).data()[0].endTime;
+  //     }
+  //     let output = document.getElementById("tag");
+
+  //     let x = this.x.animVal.getItem(this) + "px";
+  //     let y = this.y.animVal.getItem(this) + 25 + "px";
+
+  //     output.innerHTML = tag;
+  //     output.style.top = y;
+  //     output.style.left = x;
+  //     output.style.display = "block";
+  //   })
+  //   .on("mouseout", function() {
+  //     let output = document.getElementById("tag");
+  //     output.style.display = "none";
+  //   });
+
+  //   innerRects
+  //     .on("mouseover", function(e) {
+  //       //console.log(this);
+  //       let tag = "";
+
+  //       if (d3.select(this).data()[0].details != undefined) {
+  //         tag =
+  //           "Task: " +
+  //           d3.select(this).data()[0].task +
+  //           "<br/>" +
+  //           "Type: " +
+  //           d3.select(this).data()[0].type +
+  //           "<br/>" +
+  //           "Starts: " +
+  //           d3.select(this).data()[0].startTime +
+  //           "<br/>" +
+  //           "Ends: " +
+  //           d3.select(this).data()[0].endTime +
+  //           "<br/>" +
+  //           "Details: " +
+  //           d3.select(this).data()[0].details;
+  //       } else {
+  //         tag =
+  //           "Task: " +
+  //           d3.select(this).data()[0].task +
+  //           "<br/>" +
+  //           "Type: " +
+  //           d3.select(this).data()[0].type +
+  //           "<br/>" +
+  //           "Starts: " +
+  //           d3.select(this).data()[0].startTime +
+  //           "<br/>" +
+  //           "Ends: " +
+  //           d3.select(this).data()[0].endTime;
+  //       }
+  //       let output = document.getElementById("tag");
+
+  //       let x = this.x.animVal.value + this.width.animVal.value / 2 + "px";
+  //       let y = this.y.animVal.value + 25 + "px";
+
+  //       output.innerHTML = tag;
+  //       output.style.top = y;
+  //       output.style.left = x;
+  //       output.style.display = "block";
+  //     })
+  //     .on("mouseout", function() {
+  //       let output = document.getElementById("tag");
+  //       output.style.display = "none";
+  //     });
+}
+
+function makeGrid(theSidePad, theTopPad, w, h) {
+  let xAxis = d3
+    .axisBottom(timeScale)
+    .ticks(d3.timeDay, 1)
+    .tickSize(-h + theTopPad + 20, 0, 0)
+    .tickFormat(d3.timeFormat("%d %b"));
+
+  let grid = svg
+    .append("g")
+    .attr("class", "grid")
+    .attr("transform", "translate(" + theSidePad + ", " + (h - 50) + ")")
+    .call(xAxis)
+    .selectAll("text")
+    .style("text-anchor", "middle")
+    .attr("fill", "#000")
+    .attr("stroke", "none")
+    .attr("font-size", 10)
+    .attr("dy", "1em");
+}
+
+function vertLabels(
+  theGap,
+  theTopPad,
+  theSidePad,
+  theBarHeight,
+  theColorScale
+) {
+  let numOccurances = new Array();
+  let prevGap = 0;
+
+  for (let i = 0; i < categories.length; i++) {
+    numOccurances[i] = [categories[i], getCount(categories[i], catsUnfiltered)];
+  }
+
+  let axisText = svg
+    .append("g") //without doing this, impossible to put grid lines behind text
+    .selectAll("text")
+    .data(numOccurances)
+    .enter()
+    .append("text")
+    .text(function(d) {
+      return d[0];
+    })
+    .attr("x", 10)
+    .attr("y", function(d, i) {
+      if (i > 0) {
+        for (let j = 0; j < i; j++) {
+          prevGap += numOccurances[i - 1][1];
+          // console.log(prevGap);
+          return (d[1] * theGap) / 2 + prevGap * theGap + theTopPad;
+        }
+      } else {
+        return (d[1] * theGap) / 2 + theTopPad;
+      }
+    })
+    .attr("font-size", 11)
+    .attr("text-anchor", "start")
+    .attr("text-height", 14)
+    .attr("fill", function(d) {
+      for (let i = 0; i < categories.length; i++) {
+        if (d[0] == categories[i]) {
+          //  console.log("true!");
+          return d3.rgb(theColorScale(i)).darker();
+        }
+      }
+    });
+}
+
+//from this stackexchange question: http://stackoverflow.com/questions/14227981/count-how-many-strings-in-an-array-have-duplicates-in-the-same-array
+function getCounts(arr) {
+  let i = arr.length, // let to loop over
+    obj = {}; // obj to store results
+  while (i) obj[arr[--i]] = (obj[arr[i]] || 0) + 1; // count occurrences
+  return obj;
+}
+
+// get specific from everything
+function getCount(word, arr) {
+  return getCounts(arr)[word] || 0;
+}
